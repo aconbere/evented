4461355188a482acaeb1ec8d095d7b952f939c3e
Anders Conbere <aconbere@gmail.com>
2010-10-26 16:46:09 -0700

    adding bindOnce to evented
    bindOnce is an api to listen to a channel but only respond once (after
    whcih the callback is removed).

--- tests/evented_test.js
+++ tests/evented_test.js
@@ -1,6 +1,6 @@
 var minitest = require("minitest");
 var assert = require("assert");
-var Evented = require("evented").Evented;
+var Evented = require("../lib/evented").Evented;
 var sys = require("sys");
 
 minitest.context("Evented", function () {
@@ -13,13 +13,21 @@ minitest.context("Evented", function () {
     test.finished();
   });
 
-  this.assertion("subscribe should add a listener to a channel", function (test) {
+  this.assertion("bind should add a listener to a channel", function (test) {
     this.evented.bind("x", function () {});
-    assert.ok(this.evented.listeners.x);
+    assert.ok(this.evented.listeners.x.length > 0);
     test.finished();
   });
 
-  this.assertion("publish should send a message to each listener on a channel", function (test) {
+  this.assertion("bindOnce should only be called once", function (test) {
+    this.evented.bindOnce("x", function () {});
+    assert.ok(this.evented.listenersOnce.x.length > 0);
+    this.evented.trigger("x");
+    assert.ok(this.evented.listenersOnce.x.length == 0);
+    test.finished();
+  });
+
+  this.assertion("trigger should send a message to each listener on a channel", function (test) {
     var fired = false;
     var fired2 = false
     this.evented.bind("x", function () { fired = true });
